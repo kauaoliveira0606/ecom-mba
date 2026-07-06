d3f103a330cc9005c760721dbe6a5d7fe23d2e76
kauaoliveira0606 <kaua.bage@gmail.com>
2026-07-06 11:04:00 -0400

    Add /close page as exact replica of /base44

---
+++ app/close/route.ts
@@ -0,0 +1,9 @@
+import { readFile } from "fs/promises";
+import path from "path";
+
+export async function GET() {
+  const html = await readFile(path.join(process.cwd(), "app/close/source.html"), "utf-8");
+  return new Response(html, {
+    headers: { "Content-Type": "text/html; charset=utf-8" },
+  });
+}
