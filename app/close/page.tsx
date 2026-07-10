6078602ce8740d709a2476c9f28b3e2eff89aad6
kauaoliveira0606 <kaua.bage@gmail.com>
2026-07-10 12:52:38 -0400

    Replace /close with exact replica of root page placeholder

---
+++ app/close/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: "sans-serif", padding: 40 }}>
+      <h1>ecom-mba</h1>
+      <p>
+        See <a href="/base44">/base44</a> for the replica page.
+      </p>
+    </main>
+  );
+}
